dad01337c08e04a1761574227d745c8bf7a04f38
sabrina-s <tesmemepasla@gmail.com>
2020-01-29 15:42:29 +0800

    Clean up and fix file structure
    Client directory: for front-end code
    Server directory: for server-side code
    
    Reference: https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = require('./routes/routes.js')
+var path = require('path');
+var app = express();
+
+app.set('view engine', 'ejs'); // TODO: change to pug?
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/', router);
+
+module.exports = app;
