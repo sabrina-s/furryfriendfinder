ed576d6cce88d8b3d7a46e47bb78b7cc2fbe4803
sabrina-s <tesmemepasla@gmail.com>
2020-02-08 17:10:20 +0800

    Add mongoose validation for DogSchema

--- back-end/models/Dog.js
+++ back-end/models/Dog.js
@@ -2,16 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DogSchema = new Schema({
-  name: String,
-  gender: String,
-  description: String,
-  hdbApproved: Boolean,
-  birthday: Date
+  name: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female']
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  hdbApproved: {
+    type: Boolean,
+    required: true
+  },
+  birthday: Date,
+  available: {
+    type: Boolean,
+    required: true,
+    default: true
+  }
 });
 
 const Dog = mongoose.model('Dog', DogSchema);
-
-// TODO: fix this!
-// export default Dog;
 
 module.exports = Dog;
