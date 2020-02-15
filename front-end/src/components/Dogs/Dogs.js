e29160d30f979a4b294bdfe8a37a297a0c98202b
sabrina-s <tesmemepasla@gmail.com>
2020-02-15 15:16:10 +0800

    Extract data fetching into separate file

--- front-end/src/components/Dogs/Dogs.js
+++ front-end/src/components/Dogs/Dogs.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardMedia, CardContent } from '@material-ui/core';
 import { upperFirst } from 'lodash';
+import fetchDogs from '../../data/dogs';
 
 // TODO: add images for pet profiles, stored with mongoDB
 const dummyImagePath = "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
@@ -9,16 +10,14 @@ function Dogs() {
   const [dogs, setDogs] = useState([]);
 
   useEffect(() => {
-    async function fetchDogs() {
-      const response = await fetch("http://localhost:5000/dogs");
-      const json = await response.json();
-      setDogs(json);
-    }
-    fetchDogs();
+    (async () => {
+      const response = await fetchDogs();
+      setDogs(response);
+    })();
   }, []);
 
   return (
-    <div className="cards">
+    <div className="cards" data-testid="cards">
       {
         dogs.map(dog => (
           <Card className="card" key={dog._id}>
