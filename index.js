2e18cca9dda09e47d0479e44d65b7e26f0968f86
sabrina-s <tesmemepasla@gmail.com>
2020-02-19 12:23:26 +0800

    Heroku deployment fix attempt #5

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.get('/dogs', (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/public/index.html'));
   })
