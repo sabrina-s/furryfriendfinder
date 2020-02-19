9f6ca58bdd61ef0671762fdf9c157f6310216fdb
sabrina-s <tesmemepasla@gmail.com>
2020-02-19 15:24:11 +0800

    Revert path changes
    https://www.freecodecamp.org/forum/t/solved-deployment-problem-showing-backend/280178/8

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.get('/dogs', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 } else {
   app.use(express.static(path.join(__dirname, 'client/build')));
