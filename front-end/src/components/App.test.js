9d6678f74408f48796368dc95fef7f5c3e5f4606
yuhanyuhan <yuhanlian@gmail.com>
2020-02-22 13:11:59 +0800

    PLS ACCEPT TEST CASES

--- front-end/src/components/App.test.js
+++ front-end/src/components/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react'
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
 afterEach(cleanup);
 
 test('renders app header', () => {
-  const { getByText } = render(<App/>);
-  const headerElement = getByText(/Furry Friend Finder/i);
+
+  fetch.mockResponseOnce(JSON.stringify([]), {headers: { 'content-type': 'application/json' }});
+
+  let container;
+  act(() => {
+    container = render(<App/>);
+  });
+
+  const headerElement = container.getByText(/Furry Friend Finder/i);
   expect(headerElement).toBeInTheDocument();
 });
--- front-end/src/data/dogs.js
+++ front-end/src/data/dogs.js
@@ -1,10 +1,5 @@
 export default async function fetchDogs() {
-  try {
-    const response = await fetch("http://localhost:5000/dogs");
-
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await fetch("http://localhost:5000/dogs");
+  const json = await response.json();
+  return json;
 }
