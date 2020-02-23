9cbddec9fa706bd3de1aaa1f8f16cfa472107552
Amanda Oh <amandaoh@thoughtworks.com>
2020-02-23 12:52:29 +0800

    assert all dog details rendered and clean up test

--- client/src/components/Dogs/Dogs.test.js
+++ client/src/components/Dogs/Dogs.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { wait } from '@testing-library/dom';
 import { render } from '@testing-library/react';
 import Dogs from './Dogs';
@@ -9,30 +8,23 @@ beforeEach(() => {
 });
 
 test('renders dog cards', async () => {
-  const fakeDogs = [
-    {
+  const fakeDog = {
       _id: 1,
       available: false,
       name: 'Bernie',
       gender: 'male',
       description: 'Bernie is shy at first but quickly warms up to people. Once he is comfortable, he can be playful. He is extremely food-driven.',
       hdbApproved: true
-    }
-  ];
+  };
 
-  fetch.mockResponseOnce(JSON.stringify(fakeDogs), { headers: { 'content-type': 'application/json' } });
+  fetch.mockResponseOnce(JSON.stringify([fakeDog]), { headers: { 'content-type': 'application/json' } });
 
-  let container;
+  const { getByText } = render(<Dogs />);
 
-  act(() => {
-    container = render(<Dogs />);
-  })
-
-  const { queryAllByText } = container;
-
-  const bernies = await wait(() => queryAllByText(/Bernie/i).length > 0)
-
-  const bernie = queryAllByText(/Bernie/i)[0];
-
-  expect(bernie).toBeInTheDocument();
+  await wait(() => [
+    expect(getByText(fakeDog.name)).toBeInTheDocument(),
+    expect(getByText("Male")).toBeInTheDocument(),
+    expect(getByText(fakeDog.description)).toBeInTheDocument(),
+    expect(getByText("HDB approved")).toBeInTheDocument()
+  ]);
 });
