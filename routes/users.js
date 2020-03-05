736b5845b4d3ccc28e834002ab711abcf5bf88fd
sabrina-s <tesmemepasla@gmail.com>
2020-03-05 20:32:37 +0800

    Clean up User post routes

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,17 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
-const { User, validate } = require('../models/user');
+const User = require('../models/user');
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    user = new User(_.pick(req.body, ['username', 'password']));
+    await user.save();
 
-  let user = await User.findOne({ username: req.body.username });
-  if (user) return res.status(400).send('User already registered.');
-
-  user = new User(_.pick(req.body, ['username', 'password']));
-  await user.save();
-
-  res.send(_.pick(user, ['_id', 'username']));
+    res.send(_.pick(user, ['_id', 'username']));
+  } catch (error) {
+    res.send(error);
+  }
 })
 
 module.exports = router;
