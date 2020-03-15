6d6a4c3ed3b2560514137545916b8c6684ffdbdc
sabrina-s <tesmemepasla@gmail.com>
2020-03-15 15:12:02 +0800

    Fix React routers for site navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-import Dogs from './Dogs/Dogs';
 import Navbar from './Users/Navbar';
+import Dogs from './Dogs/Dogs';
+import LoginPage from './Users/LoginPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Dogs/>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Dogs} />
+          <Route path='/login' component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Users/Navbar.js
+++ client/src/components/Users/Navbar.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import LoginPage from './LoginPage';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <h2>Furry Friend Finder</h2>
-      <Router>
-        <Link to="/login">Login</Link>
-
-        <Switch>
-          <Route path="/login">
-            <LoginPage/>
-          </Route>
-        </Switch>
-      </Router>
+    <div className='navbar'>
+      <Link to='/'>
+        <h2>Furry Friend Finder</h2>
+      </Link>
+      <Link to='/login'>
+        Login
+      </Link>
     </div>
   )
 };
