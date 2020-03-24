d9d7c3ff9f3138791d0bbcda5cd60d1e503339e6
sabrina-s <tesmemepasla@gmail.com>
2020-03-24 17:36:34 +0800

    Redirect all server requests to client/public/index.html
    This is to prevent errors for when the user refreshes on a page e.g.
    `/settings`, or `/login`

--- server/app.js
+++ server/app.js
@@ -42,5 +42,6 @@ winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 // winston.add(new winston.transports.MongoDB({ db: process.env.MONGODB_URI }));
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html')));
 
 module.exports = app;
