5d50128fc90d88acd0e626848c19d1b3e23626cc
sabrina-s <tesmemepasla@gmail.com>
2020-03-25 21:27:31 +0800

    Fix placement of 'Adopt' button within Dog cards

--- client/src/components/Dogs/Dogs.jsx
+++ client/src/components/Dogs/Dogs.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardMedia, CardContent } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions } from '@material-ui/core';
 import { upperFirst } from 'lodash';
 import fetchDogs from '../../data/dogs';
 
-const dummyImagePath = "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+const dummyImagePath = 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
 
 function Dogs() {
   const [dogs, setDogs] = useState([]);
@@ -24,27 +24,31 @@ function Dogs() {
   }
 
   return (
-    <div className="cards" data-testid="cards">
+    <div className='cards' data-testid='cards'>
       {
         dogs.map(dog => (
-          <Card className="card" key={dog._id}>
+          <Card className='card' key={dog._id}>
             <CardMedia
-              className="card-image"
+              className='card-image'
               image={getImage(dog.image)}
               title={dog.name}
             />
-            <CardContent className="card-content">
+
+            <CardContent className='card-content'>
               <h3>{dog.name}</h3>
-              <div className="attributes">
+              <div className='attributes'>
                 <p>{upperFirst(dog.gender)}</p>
                 { dog.hdbApproved &&
                   <p>HDB approved</p>
                 }
               </div>
 
-              <p className="description">{dog.description}</p>
-              <button>Adopt</button>
+              <p className='description'>{dog.description}</p>
             </CardContent>
+
+            <CardActions className='card-actions'>
+              <button>Adopt</button>
+            </CardActions>
           </Card>
         ))
       }
