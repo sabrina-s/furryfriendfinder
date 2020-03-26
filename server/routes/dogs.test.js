3e44ec909fde0c59fd185b4d6cb04dab48ab7728
sabrina-s <tesmemepasla@gmail.com>
2020-03-26 16:37:11 +0800

    Improve GET /dogs test

--- server/routes/dogs.test.js
+++ server/routes/dogs.test.js
@@ -1,34 +1,49 @@
 const request = require('supertest');
 const app = require('../app');
 const testMongoDB = require('../test_helper/in_memory_mongodb_setup');
+const Dog = require('../models/dog');
 
 beforeAll(testMongoDB.setup);
 afterAll(testMongoDB.teardown);
 
+const validPayload = {
+  name: 'Tony',
+  gender: 'male',
+  description: 'Tough on the outside, soft on the inside.',
+  available: true,
+  hdbApproved: false
+};
+
+const validPayload2 = {
+  name: 'Amber',
+  gender: 'female',
+  description: 'Tough on the outside, soft on the inside.',
+  available: true,
+  hdbApproved: true
+};
+
+const invalidPayload = {
+  name: 'NewDoggo',
+  gender: 'male'
+};
+
 describe('GET /dogs', () => {
   it('should return a list of dogs', async () => {
-    await request(app)
-      .get('/api/dogs')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
+    await Dog.collection.insertMany([
+      validPayload,
+      validPayload2
+    ]);
+
+    const response = await request(app).get('/api/dogs');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body.some((dog) => dog.name === 'Amber')).toBeTruthy();
+    expect(response.body.some((dog) => dog.name === 'Tony')).toBeTruthy();
   });
 });
 
 describe('POST /dogs', () => {
-  const validPayload = {
-    name: 'NewDoggo',
-    gender: 'male',
-    description: 'Tough on the outside, soft on the inside.',
-    available: true,
-    hdbApproved: false
-  };
-
-  const invalidPayload = {
-    name: 'NewDoggo',
-    gender: 'male'
-  };
-
   it('should add new dog into database', async () => {
     await request(app)
       .post('/api/dogs')
